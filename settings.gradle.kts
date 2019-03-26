8e4ee3eccf882524c7a6114ae974dea57da7f799
iwauo <iwauo.tajima@gmail.com>
2019-03-27 03:43:56 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "laplacian-arch.model.service-api-arch"
+pluginManagement {
+    repositories {
+        maven(url = "../mvn-repo/")
+        maven(url = "https://raw.github.com/nabla-squared/mvn-repo/master/")
+        gradlePluginPortal()
+        jcenter()
+    }
+}
